fa8589e46aad2e72953264d13ebba1fcd8a87074
bitnoony <noony74@daum.net>
2023-11-20 16:27:39 +0900

    완성

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {arrPokemon} from '../stores/pokestore';
+    // console.log($arrPokemon);
+    import PokemonCard from '../components/pokemonCard.svelte';
+
+    let searchTerm = '';
+    let filteredPokemon = [];
+
+    $: {
+        // console.log(searchTerm);
+        if (searchTerm) {
+           filteredPokemon = $arrPokemon.filter(pokemon => pokemon.name.includes(searchTerm));
+        } else {
+           filteredPokemon = [...$arrPokemon];
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>포켓몬 위키</title>
+</svelte:head>
+
+
+<h1 class="text-4xl text-center my-8 font-bold underline">
+포켓몬 위키 사이트</h1>
+
+<input type="text" class="w-full rounded-md text-lg p-4 mb-8 border-2 border-gray-200"
+  bind:value={searchTerm}
+  placeholder="포켓몬 검색" 
+  />
+  
+  <div class="grid gap-4 md:grid-cols-3 grid-cols-2">
+{#each filteredPokemon as pokemon }
+    <PokemonCard {pokemon} />
+{/each}
+</div>
---
+++ src/routes/pokemon/[id]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let data;
+    const { pokemon } = data;
+    import {arrPokemon} from '../../../stores/pokestore';
+</script>
+
+<h1 class="text-4xl text-center text-red-500">포켓몬 상세페이지</h1>
+
+<div class="flex flex-col items-center text-2xl">
+    <h1 class="text-4xl text-center my-8">{pokemon.name}</h1>
+    <h1 class="text-4xl text-center my-8">{$arrPokemon[pokemon.id - 1].name}</h1>
+    <p>타입: {pokemon.types[0].type.name}</p>
+    <p>키: {pokemon.height}</p>
+    <p>무게: {pokemon.weight}</p>
+    <img class="h-40 w-40" src={pokemon.sprites['front_default']} 
+    alt={pokemon.name} />
+</div>
